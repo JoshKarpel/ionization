8e52a5d9ae3cf414d83dc98963332a7813e4d696
JoshKarpel <josh.karpel@gmail.com>
2017-08-16 15:47:55 -0500

    RAM usage investigations.

---
+++ dev/cluster/sim_ram.py
@@ -0,0 +1,62 @@
+import logging
+import os
+import psutil
+import functools
+import subprocess
+import time
+
+import numpy as np
+
+import simulacra as si
+from simulacra.units import *
+
+import ionization as ion
+
+FILE_NAME = os.path.splitext(os.path.basename(__file__))[0]
+OUT_DIR = os.path.join(os.getcwd(), 'out', FILE_NAME)
+
+
+PLOT_KWARGS = dict(
+    target_dir = OUT_DIR,
+    img_format = 'png',
+    fig_dpi_scale = 6,
+)
+
+
+def run_sim_in_proc(R, ppR, L, T):
+    process = subprocess.Popen(['python', 'sim_ram_runner.py', str(R), str(ppR), str(L), str(T)])
+    return process
+
+
+if __name__ == '__main__':
+    with si.utils.LogManager('simulacra', 'ionization', stdout_logs = True, stdout_level = logging.INFO) as logger:
+        R = 250
+        ppR = 8
+        L = 500
+        T = 50
+
+        times = []
+        rss = []
+        proc = run_sim_in_proc(R, ppR, L, T)
+        ps_proc = psutil.Process(proc.pid)
+
+        while proc.poll() is None:
+            try:
+                t, mem = time.time(), ps_proc.memory_info().rss
+                times.append(t)
+                rss.append(mem)
+            except psutil.NoSuchProcess:
+                pass
+
+            time.sleep(10 * usec)
+
+        times = np.array(times) - times[0]
+
+        si.vis.xy_plot(
+            f'rss_vs_time__R={R}_ppR={ppR}_L={L}_T={T}',
+            times,
+            rss,
+            x_label = 'Time', x_unit = 's',
+            y_label = 'RSS (MB)', y_unit = (1024 ** 2),
+            **PLOT_KWARGS,
+        )
---
+++ dev/cluster/sim_ram_runner.py
@@ -0,0 +1,46 @@
+import time
+
+time.sleep(5)
+
+import sys
+import logging
+import os
+
+import simulacra as si
+from simulacra.units import *
+
+import ionization as ion
+
+FILE_NAME = os.path.splitext(os.path.basename(__file__))[0]
+OUT_DIR = os.path.join(os.getcwd(), 'out', FILE_NAME)
+
+time.sleep(5)
+
+if __name__ == '__main__':
+    with si.utils.LogManager('simulacra', 'ionization', stdout_logs = True, stdout_level = logging.INFO) as logger:
+        R, ppR, L, T = sys.argv[1:]
+
+        R = int(R)
+        ppR = int(ppR)
+        L = int(L)
+        T = int(T)
+
+        sim = ion.SphericalHarmonicSpecification(
+            f'ram_test__R={R}_ppR={ppR}_L={L}',
+            r_bound = R * bohr_radius,
+            r_points = R * ppR, l_bound = L,
+            use_numeric_eigenstates = True,
+            numeric_eigenstate_max_angular_momentum = 20,
+            numeric_eigenstate_max_energy = 50 * eV,
+            time_initial = 0, time_final = T * asec, time_step = 1 * asec,
+            # electric_potential = ion.SineWave.from_photon_energy(10 * eV, amplitude = 1 * atomic_electric_field),
+            store_data_every = 1,
+        ).to_simulation()
+
+        time.sleep(5)
+
+        sim.run_simulation()
+
+        sim.info().log()
+
+        time.sleep(5)
