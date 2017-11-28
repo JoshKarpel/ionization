ed929359c43a511f67e96481789f8e288262ade6
JoshKarpel <josh.karpel@gmail.com>
2017-11-27 22:47:41 -0600

    Better plotting options for hydrogen-Bessel velocity kernel.

--- science/integrodiff/hydrogen_bessel_velocity_kernel.py
+++ science/integrodiff/hydrogen_bessel_velocity_kernel.py
@@ -63,35 +63,35 @@ def kernel(time_difference, alpha, omega_b = ion.HydrogenBoundState(1, 0).energy
 
 if __name__ == '__main__':
     with LOGMAN as logger:
-        tds = np.linspace(0, 500, 100) * u.asec
-        alphas = np.linspace(0, 10, 100) * u.bohr_radius
+        tds = np.linspace(0, 500, 200) * u.asec
+        alphas = np.linspace(-20, 20, 200) * u.bohr_radius
 
-        ks = np.linspace(0, 20, 1000) / u.bohr_radius
-
-        si.vis.xy_plot(
-            'theta_k_integral',
-            ks,
-            [theta_k_integral(k, 0) for k in ks],
-            [theta_k_integral(k, .1 * u.bohr_radius) for k in ks],
-            [theta_k_integral(k, .2 * u.bohr_radius) for k in ks],
-            [theta_k_integral(k, 1 * u.bohr_radius) for k in ks],
-            line_labels = ['0', '.1', '.2', '1'],
-            x_unit = 'per_bohr_radius',
-            x_label = 'k',
-            **PLOT_KWARGS,
-        )
-
-        kern_integrand = kernel_integrand(ks, time_difference = 0, alpha = .1 * u.bohr_radius)
-
-        si.vis.xy_plot(
-            'integrand',
-            ks,
-            np.abs(kern_integrand),
-            np.real(kern_integrand),
-            np.imag(kern_integrand),
-            x_unit = 'per_bohr_radius',
-            **PLOT_KWARGS,
-        )
+        # ks = np.linspace(0, 20, 1000) / u.bohr_radius
+        #
+        # si.vis.xy_plot(
+        #     'theta_k_integral',
+        #     ks,
+        #     [theta_k_integral(k, 0) for k in ks],
+        #     [theta_k_integral(k, .1 * u.bohr_radius) for k in ks],
+        #     [theta_k_integral(k, .2 * u.bohr_radius) for k in ks],
+        #     [theta_k_integral(k, 1 * u.bohr_radius) for k in ks],
+        #     line_labels = ['0', '.1', '.2', '1'],
+        #     x_unit = 'per_bohr_radius',
+        #     x_label = 'k',
+        #     **PLOT_KWARGS,
+        # )
+        #
+        # kern_integrand = kernel_integrand(ks, time_difference = 0, alpha = .1 * u.bohr_radius)
+        #
+        # si.vis.xy_plot(
+        #     'integrand',
+        #     ks,
+        #     np.abs(kern_integrand),
+        #     np.real(kern_integrand),
+        #     np.imag(kern_integrand),
+        #     x_unit = 'per_bohr_radius',
+        #     **PLOT_KWARGS,
+        # )
 
         td_mesh, alpha_mesh = np.meshgrid(tds, alphas, indexing = 'ij')
         kernel_mesh = np.empty_like(td_mesh, dtype = np.complex128)
@@ -99,8 +99,7 @@ if __name__ == '__main__':
             for j, alpha in enumerate(alphas):
                 kernel_mesh[i, j] = kernel(td, alpha)
 
-        kernel_norm = kernel_mesh[0, 0]
-        print(kernel_norm)
+        kernel_norm = kernel(np.array([0]), np.array([0]))
 
         si.vis.xyz_plot(
             'kernel_vs_td_and_alpha',
