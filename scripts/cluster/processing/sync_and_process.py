14ebf53db8a5504c4b1c2710c89287a6628c8755
JoshKarpel <josh.karpel@gmail.com>
2018-01-01 16:36:59 -0600

    Fixed header in processing report

--- scripts/cluster/processing/sync_and_process.py
+++ scripts/cluster/processing/sync_and_process.py
@@ -97,7 +97,7 @@ def generate_processing_report(job_processors):
     total_runtime = sum((jp.running_time for jp in job_processors), dt.timedelta())
     len_job_num = len(str(total_jobs)) + 1
 
-    header = f' {"Job Name".center(len_of_longest_jp_name)} │ Finished │ Total │ Runtime'
+    header = f' {"Job Name".center(len_of_longest_jp_name)} │ {"Processed".center(len_job_num)} │ {"Total".center(len_job_num)} │ Runtime'
 
     bar = ''.join('─' if char != '│' else '┼' for char in header)
 
