43891d54d42e4c5998a18052e2edb46d76cc0df6
JoshKarpel <josh.karpel@gmail.com>
2018-01-07 10:25:38 -0600

    Hopefully the final fix for the report text alignment...

--- scripts/cluster/processing/sync_and_process.py
+++ scripts/cluster/processing/sync_and_process.py
@@ -97,10 +97,12 @@ def generate_processing_report(job_processors):
     total_runtime = sum((jp.running_time for jp in job_processors), dt.timedelta())
     len_job_num = len(str(total_jobs)) + 1
 
-    job_name = 'Job Name'.center(len_of_longest_jp_name)
-    processed = 'Processed'.center(len_job_num)
-    total = 'Total'.center(len_job_num)
+    processed = 'Processed'
+    total = 'Total'
     len_job_num = max(len_job_num, len(processed), len(total))
+    job_name = 'Job Name'.center(len_of_longest_jp_name)
+    processed = processed.center(len_job_num)
+    total = total.center(len_job_num)
     header = f' {job_name} │ {processed} │ {total} │ Runtime'
 
     bar = ''.join('─' if char != '│' else '┼' for char in header)
