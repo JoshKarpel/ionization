571878fd06ff2c62d2426df1dd7b4d6709f6b8c9
JoshKarpel <josh.karpel@gmail.com>
2018-01-21 14:33:41 -0600

    Worked on autoscans. Refactored, pushed new plot_parameter selection to 2d scans as well

--- dev/cluster_autosummaries/autosummary.py
+++ dev/cluster_autosummaries/autosummary.py
@@ -12,11 +12,11 @@ FILE_NAME = os.path.splitext(os.path.basename(__file__))[0]
 OUT_DIR = os.path.join(os.getcwd(), 'out', FILE_NAME)
 
 if __name__ == '__main__':
-    with si.utils.LogManager('simulacra', 'ionization', stdout_logs = True, stdout_level = logging.DEBUG) as logger:
+    with si.utils.LogManager('simulacra', 'ionization', stdout_logs = True, stdout_level = logging.INFO) as logger:
         # jp = clu.JobProcessor.load('paper_tdse_test_small_mesh.job')
-        jp = clu.JobProcessor.load('tdse_gaussian_pw_scan.job')
+        # jp = clu.JobProcessor.load('tdse_gaussian_pw_scan.job')
         # jp = clu.JobProcessor.load('tdse_heatmap_pw_vs_amp__lin.job')
-        # jp = clu.JobProcessor.load('tdse_heatmap_pw_vs_flu__lin.job')  # doesn't generate log y plots because we have 0 fluence in the dataset - good to realize!
+        jp = clu.JobProcessor.load('tdse_heatmap_pw_vs_flu__lin.job')  # doesn't generate log y plots because we have 0 fluence in the dataset - good to realize!
 
         print(jp)
         jp.job_dir_path = OUT_DIR
