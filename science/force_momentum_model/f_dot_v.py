666a06dfcc4588a29edd661bed0de0e9d66b82a9
JoshKarpel <josh.karpel@gmail.com>
2018-02-05 17:35:28 -0600

    Solving power model for cosine/sine pulses

--- science/force_momentum_model/f_dot_v.py
+++ science/force_momentum_model/f_dot_v.py
@@ -33,42 +33,80 @@ def pulse_identifier(pulse):
     return f'{pulse.__class__.__name__}_pw={u.uround(pulse.pulse_width, u.asec)}as_cep={u.uround(pulse.phase, u.pi)}pi'
 
 
-def plot_f_and_v(pulse, times):
-    efield = pulse.get_electric_field_amplitude(times)
-    afield = pulse.get_vector_potential_amplitude_numeric_cumulative(times)
+def get_power(pulse, times, lookback = None):
+    force = CHARGE * pulse.get_electric_field_amplitude(times)
+    velocity = -CHARGE * pulse.get_vector_potential_amplitude_numeric_cumulative(times) / MASS
 
-    force = CHARGE * efield
-    velocity = -CHARGE * afield / MASS
-    power = force * velocity
-    energy = integ.cumtrapz(y = power, x = times, initial = 0)
+    if lookback is not None:
+        dt = times[1] - times[0]
+        window = int(lookback / dt)
 
-    # https://stackoverflow.com/questions/12709853/python-running-cumulative-sum-with-a-given-window
-    dt = times[1] - times[0]
-    window = int(150 * u.asec / dt)
-    energy_lookback = np.cumsum(power)
-    energy_lookback[window:] = energy_lookback[window:] - energy_lookback[:-window]
-    energy_lookback *= dt
+        velocity[window:] = velocity[window:] - velocity[:-window]
+
+    return force * velocity
+
+
+def plot_f_and_v(cos_pulse, sin_pulse, times):
+    pulses = [cos_pulse, sin_pulse]
+
+    powers = [get_power(pulse, times) for pulse in pulses]
 
     si.vis.xy_plot(
-        pulse_identifier(pulse[0]),
+        'F_AND_V___' + '__'.join(pulse_identifier(pulse[0]) for pulse in pulses),
         times,
-        # force / u.atomic_force,
-        # velocity / u.atomic_velocity,
-        power / u.atomic_power,
-        energy / u.hartree,
-        energy_lookback / u.hartree,
+        *[power / u.atomic_power for power in powers],
         line_labels = [
-            # r'$ F(t) $',
-            # r'$ v(t) $',
-            r'$ F(t) \cdot v(t) $',
-            r"$ \int_{-\infty}^t F(t') \cdot v(t') \, dt' $",
-            r"$ \int_{-T_{\mathrm{cl}}}^t F(t') \cdot v(t') \, dt' $",
+            r'$ F(t) \cdot v(t), \, \varphi = 0$',
+            r'$ F(t) \cdot v(t), \, \varphi = \pi / 2$',
         ],
         x_unit = 'asec',
         x_label = r'$ t $',
-        legend_on_right = True,
-        title = pulse_identifier(pulse[0]),
+        # legend_on_right = True,
+        title = '\n'.join(pulse_identifier(pulse[0]) for pulse in pulses),
         **PLOT_KWARGS,
+    )
+
+
+def solve_power_model(pulse, times, lookback = None):
+    power = get_power(pulse, times, lookback = lookback)
+
+    rate = -power / u.hartree
+
+    rate_integral = integ.cumtrapz(rate, times, initial = 0)
+
+    return np.exp(rate_integral)
+
+
+def plot_power_model(pulses, times):
+    bs = [solve_power_model(pulse, times) for pulse in pulses]
+    abs_b_squared = [np.abs(b) ** 2 for b in bs]
+
+    bs_with_window = [solve_power_model(pulse, times, lookback = 150 * u.asec) for pulse in pulses]
+    abs_b_squared_with_lookback = [np.abs(b) ** 2 for b in bs_with_window]
+
+    si.vis.xy_plot(
+        'B___' + '__'.join(pulse_identifier(pulse[0]) for pulse in pulses),
+        times,
+        *abs_b_squared,
+        *abs_b_squared_with_lookback,
+        line_labels = [
+            r'$ \varphi = 0 $',
+            r'$ \varphi = 0 $, w/LB',
+            r'$ \varphi = \pi / 2 $',
+            r'$ \varphi = \pi / 2 $, w/LB',
+        ],
+        line_kwargs = [
+            {'color': 'C0', 'linestyle': '--'},
+            {'color': 'C0', 'linestyle': '-'},
+            {'color': 'C1', 'linestyle': '--'},
+            {'color': 'C1', 'linestyle': '-'},
+        ],
+        x_unit = 'asec',
+        x_label = r'$ t $',
+        y_label = r'$ \left| b(t) \right|^2 $',
+        # legend_on_right = True,
+        title = '\n'.join(pulse_identifier(pulse[0]) for pulse in pulses),
+        **PLOT_KWARGS
     )
 
 
@@ -78,18 +116,16 @@ if __name__ == '__main__':
             ion.potentials.SincPulse(phase = 0),
             ion.potentials.SincPulse(phase = u.pi / 2),
         ]
-        for pulse in pulses:
-            times = np.linspace(-35 * pulse.pulse_width, 35 * pulse.pulse_width, 10000)
-            pulse = ion.potentials.DC_correct_electric_potential(pulse, times)
-            print(pulse.info())
-            plot_f_and_v(pulse, times)
+        times = np.linspace(-20 * pulses[0].pulse_width, 20 * pulses[0].pulse_width, 10000)
+        pulses = [ion.potentials.DC_correct_electric_potential(pulse, times) for pulse in pulses]
+        plot_f_and_v(*pulses, times)
+        plot_power_model(pulses, times)
 
         pulses = [
-            ion.potentials.GaussianPulse.from_number_of_cycles(phase = 0, number_of_cycles = 3),
-            ion.potentials.GaussianPulse.from_number_of_cycles(phase = u.pi / 2, number_of_cycles = 3),
+            ion.potentials.GaussianPulse.from_number_of_cycles(number_of_cycles = 3, phase = 0),
+            ion.potentials.GaussianPulse.from_number_of_cycles(number_of_cycles = 3, phase = u.pi / 2),
         ]
-        for pulse in pulses:
-            times = np.linspace(-5 * pulse.pulse_width, 5 * pulse.pulse_width, 10000)
-            pulse = ion.potentials.DC_correct_electric_potential(pulse, times)
-            print(pulse.info())
-            plot_f_and_v(pulse, times)
+        times = np.linspace(-5 * pulses[0].pulse_width, 5 * pulses[0].pulse_width, 10000)
+        pulses = [ion.potentials.DC_correct_electric_potential(pulse, times) for pulse in pulses]
+        plot_f_and_v(*pulses, times)
+        plot_power_model(pulses, times)
