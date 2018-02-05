936b17a445091803401aa789eae470c398073908
JoshKarpel <josh.karpel@gmail.com>
2018-02-05 09:52:37 -0600

    Playing with more modulation depths

--- dev/plotting/modulation_depth_heatmap_testing.py
+++ dev/plotting/modulation_depth_heatmap_testing.py
@@ -11,6 +11,7 @@ import ionization as ion
 import ionization.cluster as iclu
 
 import matplotlib.pyplot as plt
+import matplotlib.colors
 
 FILE_NAME = os.path.splitext(os.path.basename(__file__))[0]
 OUT_DIR = os.path.join(os.getcwd(), 'out', FILE_NAME)
@@ -37,14 +38,59 @@ def cos_over_sin(cosine, sine):
     return cosine / sine
 
 
+def sin_over_cos(cosine, sine):
+    return sine / cosine
+
+
 def diff_ionized_over_avg_ionized(cosine, sine):
     return 2 * (cosine - sine) / (2 - cosine - sine)
+
+
+def cos_over_sin_minus_sin_over_cos(cosine, sine):
+    return (cosine / sine) - (sine / cosine)
+
+
+def log_over_log(cosine, sine):
+    return (np.log10(cosine) - np.log10(sine)) / (np.log10(cosine) + np.log10(sine))
+
+
+def log_over_log_normed(cosine, sine):
+    return (np.log10(cosine) - np.log10(sine)) / np.sqrt((np.log10(cosine) ** 2) + (np.log10(sine) ** 2))
+
+
+def hyperbolic(cosine, sine):
+    sign = -np.sign(cosine - sine)
+
+    diff_abs = np.abs(cosine - sine)
+    diff_frac = np.abs(cosine / sine)
+    diff_frac_log = np.abs(np.log(np.abs(cosine / sine)))
+
+    always_geq1_diff_frac = np.where(diff_frac >= 1, diff_frac, 1 / diff_frac)
+
+    # diff_abs_norm = diff_abs / np.nanmax(diff_abs)
+    # diff_frac_norm = diff_frac / np.nanmax(diff_frac)
+    # diff_frac_log_norm = diff_frac_log / np.nanmax(diff_frac_log)
+
+    # C = sign * np.abs((diff_abs_norm - diff_frac_norm) / (diff_abs_norm + diff_frac_norm))
+
+    # print(diff_frac, diff_frac_log)
+    C = sign * np.where(always_geq1_diff_frac <= 10, always_geq1_diff_frac / 10, diff_frac_log / np.max(diff_frac_log))
+    # C /= np.nanmax(C)
+
+    # C = sign * np.maximum(diff_frac, diff_frac_log)
+
+    return C
 
 
 FUNC_TO_TEX = {
     diff_over_avg: r'$ M = 2 \frac{C-S}{C+S} $',
     cos_over_sin: r'$ M = \frac{C}{S} $',
+    sin_over_cos: r'$ M = \frac{S}{C} $',
     diff_ionized_over_avg_ionized: r'$ M = 2\frac{C - S}{2 - S - C}$',
+    cos_over_sin_minus_sin_over_cos: r'$ M = \frac{C}{S} - \frac{S}{C} $',
+    log_over_log: r'$ M = \frac{\log C - \log S}{\log C + \log S} $',
+    log_over_log_normed: r'$ M = \frac{\log C - \log S}{\sqrt{(\log C)^2 + (\log S)^2}} $',
+    hyperbolic: r'$ M = $ Crazy Stuff',
 }
 
 LOG_TO_FUNC = {
@@ -83,6 +129,8 @@ def make_heatmap(modulation_depth_func, plt_kwargs):
             x_log_axis = log_xy,
             y_log_axis = log_xy,
             z_log_axis = log_z,
+            # z_lower_limit = -np.max(np.abs(z_mesh)),
+            # z_upper_limit = np.max(np.abs(z_mesh)),
             save_on_exit = False,
             close_after_exit = False,
             **plt_kwargs,
@@ -124,8 +172,35 @@ def make_heatmap(modulation_depth_func, plt_kwargs):
         figman.cleanup()
 
 
+# class HyperbolicColormap(matplotlib.colors.Colormap):
+#     def __init__(self):
+#         self.name = 'hyperbolic'
+#         self.N = 256
+#
+#     def __call__(self, x, alpha = 1, bytes = False):
+#         diff_abs = np.abs(x)
+#         diff_
+
 if __name__ == '__main__':
     with LOGMAN as logger:
         for func in FUNC_TO_TEX.keys():
             for plt_kwargs in PLOT_KWARGS:
                 make_heatmap(func, plt_kwargs)
+
+        x = np.linspace(0.0001, 1, 100)[1:]
+        C_max = 20
+        Cs = np.linspace(.01, 1, 10)
+        curves = [C / x for C in Cs]
+        print(curves)
+
+        si.vis.xy_plot(
+            'curves',
+            x,
+            *curves,
+            x_label = 'abs diff',
+            y_label = 'frac diff',
+            line_labels = Cs,
+            line_kwargs = [{'color': str(C / C_max)} for C in Cs],
+            y_log_axis = True,
+            **PLOT_KWARGS[0]
+        )
---
+++ science/fields/bandwidth_vs_pulse_width.py
@@ -0,0 +1,89 @@
+import logging
+import os
+import itertools
+
+import numpy as np
+
+import simulacra as si
+import simulacra.units as u
+
+import ionization as ion
+
+import matplotlib.pyplot as plt
+
+FILE_NAME = os.path.splitext(os.path.basename(__file__))[0]
+OUT_DIR = os.path.join(os.getcwd(), 'out', FILE_NAME)
+
+LOGMAN = si.utils.LogManager('simulacra', 'ionization', stdout_level = logging.DEBUG)
+
+PLOT_KWARGS = dict(
+    target_dir = OUT_DIR,
+    img_format = 'png',
+    fig_dpi_scale = 6,
+)
+
+
+def bandwidth_plot():
+    pulse_widths = np.linspace(50, 1100, 1001)[1:] * u.asec
+    pulses = [ion.potentials.SincPulse(pulse_width = pw) for pw in pulse_widths]
+
+    f_min = np.array([p.frequency_min for p in pulses])
+    f_carrier = np.array([p.frequency_carrier for p in pulses])
+    f_max = np.array([p.frequency_max for p in pulses])
+
+    figman = si.vis.xy_plot(
+        'bandwidth',
+        pulse_widths,
+        f_carrier,
+        x_label = r'Pulse Width $ \tau $',
+        y_label = r'Frequency $ f $',
+        x_unit = 'asec',
+        y_unit = 'THz',
+        title = 'Sinc Pulse Bandwidth',
+        line_kwargs = [dict(color = 'black', linewidth = 2)],
+        font_size_axis_labels = 12,
+        save_on_exit = False,
+        close_after_exit = False,
+        **PLOT_KWARGS)
+    ax_freq = figman.fig.axes[0]
+    ax_energy = ax_freq.twinx()
+    ax_energy.set_ylabel(r'Photon Energy $E$ (eV)', fontsize = 12)
+
+    ax_freq.fill_between(
+        pulse_widths / u.asec,
+        f_min / u.THz,
+        f_max / u.THz,
+        facecolor = 'grey',
+        edgecolor = 'black',
+        linewidth = 2,
+    )
+
+    energies = [0] + [ion.states.HydrogenBoundState(n).energy for n in range(1, 5)]
+    transition_energies = set(abs(a - b)
+                              for a, b in itertools.product(energies, repeat = 2))
+
+    print([e / u.eV for e in transition_energies])
+
+    for transition_energy in transition_energies:
+        ax_energy.axhline(
+            transition_energy / u.eV,
+            linewidth = 1,
+            linestyle = '-',
+            color = 'blue',
+            alpha = 0.8,
+        )
+
+    y_lim_freq = 1.1 * np.max(f_max)
+    y_lim_energy = u.h * y_lim_freq
+
+    ax_freq.set_xlim(0, 1000)
+    ax_freq.set_ylim(0, y_lim_freq / u.THz)
+    ax_energy.set_ylim(0, y_lim_energy / u.eV)
+
+    figman.save()
+    figman.cleanup()
+
+
+if __name__ == '__main__':
+    with LOGMAN as logger:
+        bandwidth_plot()
