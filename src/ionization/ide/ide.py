Fixed missing factor in LandauRate
Worked on making bauer_1999 into a Jupyter notebook

Added progress bar for IDE sims

Refactored CylindricalSlice and SphericalSlice kinetic energy operators into smaller functions

src/ionization/ide/ide.py
@@ -2,6 +2,8 @@ import logging
 import datetime
 from typing import Union, Callable
 
+from tqdm import tqdm
+
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,7 +13,7 @@ import scipy.interpolate as interp
 import simulacra as si
 import simulacra.units as u
 
-from .. import core, potentials
+from .. import core, potentials, tunneling
 
 from . import kernels, evolution_methods
 
@@ -109,7 +111,7 @@ class IntegroDifferentialEquationSimulation(si.Simulation):
             self.interpolated_vector_potential,
         )
 
-    def run(self, callback: Callable = None):
+    def run(self, progress_bar: bool = False, callback: Callable = None):
         """
         Run the IDE simulation by repeatedly evolving it forward in time.
 
@@ -122,12 +124,17 @@ class IntegroDifferentialEquationSimulation(si.Simulation):
         logger.info(f'Performing time evolution on {self.name} ({self.file_name}), starting from time index {self.time_index}')
         self.status = si.Status.RUNNING
 
+        if progress_bar:
+            num_asecs_remaining = int((self.spec.time_final - self.times[-1]) / u.asec)
+            current_asecs_remaining = num_asecs_remaining
+            pbar = tqdm(total = num_asecs_remaining, ascii = True, ncols = 80)
+
         while self.time < self.spec.time_final:
             new_b, new_t = self.spec.evolution_method.evolve(self, self.b, self.times, self.time_step)
             dt = new_t - self.time
 
-            efield_amplitude = self.spec.electric_potential.get_electric_field_amplitude(self.time + (dt / 2))
-            new_b *= np.exp(-dt * self.spec.additional_decay_function(efield_amplitude))
+            tunneling_rate = self.spec.tunneling_model.tunneling_rate(self, self.spec.electric_potential, self.time + (dt / 2))
+            new_b *= np.exp(dt * tunneling_rate)
 
             self.b.append(new_b)
             self.times.append(new_t)
@@ -146,6 +153,18 @@ class IntegroDifferentialEquationSimulation(si.Simulation):
                     self.latest_checkpoint_time = now
                     logger.info(f'Checkpointed {self} at time index {self.time_index} ({round(completion_percent)}%)')
                     self.status = si.Status.RUNNING
+
+            try:
+                new_asecs_remaining = int((self.spec.time_final - self.times[-1]) / u.asec)
+                pbar.update(current_asecs_remaining - new_asecs_remaining)
+                current_asecs_remaining = new_asecs_remaining
+            except NameError:
+                pass
+
+        try:
+            pbar.close()
+        except NameError:
+            pass
 
         self.b = np.array(self.b)
         self.times = np.array(self.times)
@@ -306,37 +325,35 @@ class IntegroDifferentialEquationSimulation(si.Simulation):
             figman.name += postfix
 
 
-def return_zero(*args, **kwargs):
-    return 0
-
-
 class IntegroDifferentialEquationSpecification(si.Specification):
     """
     A Specification for an :class:`IntegroDifferentialEquationSimulation`.
     """
     simulation_type = IntegroDifferentialEquationSimulation
 
-    def __init__(self,
-                 name,
-                 time_initial: float = 0 * u.asec,
-                 time_final: float = 200 * u.asec,
-                 time_step: float = 1 * u.asec,
-                 test_mass: float = u.electron_mass,
-                 test_charge: float = u.electron_charge,
-                 b_initial: Union[float, complex] = 1,
-                 integral_prefactor: float = -((u.electron_charge / u.hbar) ** 2),
-                 electric_potential: potentials.PotentialEnergy = potentials.NoElectricPotential(),
-                 electric_potential_dc_correction: bool = False,
-                 kernel: kernels.Kernel = kernels.LengthGaugeHydrogenKernel(),
-                 integration_method: str = 'simpson',
-                 evolution_method: evolution_methods.EvolutionMethod = evolution_methods.RungeKuttaFourMethod(),
-                 evolution_gauge: str = 'LEN',
-                 checkpoints: bool = False,
-                 checkpoint_every: datetime.timedelta = datetime.timedelta(hours = 1),
-                 checkpoint_dir: str = None,
-                 store_data_every: int = 1,
-                 additional_decay_function: Callable = None,
-                 **kwargs):
+    def __init__(
+            self,
+            name: str,
+            time_initial: float = 0 * u.asec,
+            time_final: float = 200 * u.asec,
+            time_step: float = 1 * u.asec,
+            test_mass: float = u.electron_mass,
+            test_charge: float = u.electron_charge,
+            b_initial: Union[float, complex] = 1,
+            integral_prefactor: float = -((u.electron_charge / u.hbar) ** 2),
+            electric_potential: potentials.PotentialEnergy = potentials.NoElectricPotential(),
+            electric_potential_dc_correction: bool = False,
+            kernel: kernels.Kernel = kernels.LengthGaugeHydrogenKernel(),
+            integration_method: str = 'simpson',
+            evolution_method: evolution_methods.EvolutionMethod = evolution_methods.RungeKuttaFourMethod(),
+            evolution_gauge: str = 'LEN',
+            checkpoints: bool = False,
+            checkpoint_every: datetime.timedelta = datetime.timedelta(hours = 1),
+            checkpoint_dir: str = None,
+            store_data_every: int = 1,
+            tunneling_model: tunneling.TunnelingModel = tunneling.NoTunneling(),
+            ionization_potential = -u.rydberg,
+            **kwargs):
         """
         The differential equation should be of the form
         da/dt = prefactor * f(t) * integral[ f(t') * a(t') * kernel(t - t', ...)
@@ -407,9 +424,8 @@ class IntegroDifferentialEquationSpecification(si.Specification):
 
         self.store_data_every = store_data_every
 
-        if additional_decay_function is None:
-            additional_decay_function = return_zero
-        self.additional_decay_function = additional_decay_function
+        self.tunneling_model = tunneling_model
+        self.ionization_potential = ionization_potential
 
     def info(self):
         info = super().info()

src/ionization/mesh/mesh_operators.py
@@ -217,7 +217,7 @@ class Operators(abc.ABC):
 
 class LineLengthGaugeOperators(Operators):
     @si.utils.memoize
-    def kinetic_energy(self, mesh) -> SumOfOperators:
+    def kinetic_energy(self, mesh: 'meshes.LineMesh') -> SumOfOperators:
         prefactor = -(u.hbar ** 2) / (2 * mesh.spec.test_mass * (mesh.delta_z ** 2))
 
         diag = -2 * prefactor * np.ones(len(mesh.z_mesh), dtype = np.complex128)
@@ -227,14 +227,14 @@ class LineLengthGaugeOperators(Operators):
 
         return SumOfOperators(DotOperator(matrix, wrapping_direction = None))
 
-    def interaction_hamiltonian(self, mesh) -> SumOfOperators:
+    def interaction_hamiltonian(self, mesh: 'meshes.LineMesh') -> SumOfOperators:
         epot = mesh.spec.electric_potential(t = mesh.sim.time, distance_along_polarization = mesh.z_mesh, test_charge = mesh.spec.test_charge)
 
         matrix = sparse.diags((epot,), offsets = (0,))
 
         return SumOfOperators(DotOperator(matrix, wrapping_direction = None))
 
-    def split_interaction_operators(self, mesh: 'meshes.QuantumMesh', interaction_hamiltonian, tau: complex) -> Tuple[MeshOperator, ...]:
+    def split_interaction_operators(self, mesh: 'meshes.LineMesh', interaction_hamiltonian, tau: complex) -> Tuple[MeshOperator, ...]:
         matrix = interaction_hamiltonian.operators[0].matrix.data[0]  # I happen to know that it's 1D, but this is a little ugly
 
         return DotOperator(
@@ -253,19 +253,19 @@ class LineLengthGaugeOperators(Operators):
 
 class LineVelocityGaugeOperators(LineLengthGaugeOperators):
     @si.utils.memoize
-    def interaction_hamiltonian_matrices_without_field(self, mesh) -> Tuple['meshes.OperatorMatrix', ...]:
+    def interaction_hamiltonian_matrices_without_field(self, mesh: 'meshes.LineMesh') -> Tuple['meshes.OperatorMatrix', ...]:
         prefactor = 1j * u.hbar * (mesh.spec.test_charge / mesh.spec.test_mass) / (2 * mesh.delta_z)
         offdiag = prefactor * np.ones(mesh.spec.z_points - 1, dtype = np.complex128)
 
         return sparse.diags((-offdiag, offdiag), offsets = (-1, 1)),
 
-    def interaction_hamiltonian(self, mesh) -> SumOfOperators:
+    def interaction_hamiltonian(self, mesh: 'meshes.LineMesh') -> SumOfOperators:
         matrices = self.interaction_hamiltonian_matrices_without_field(mesh)
         vector_potential_amp = mesh.spec.electric_potential.get_vector_potential_amplitude_numeric(mesh.sim.times_to_current)  # decouple
 
         return SumOfOperators(*(DotOperator(vector_potential_amp * matrix, wrapping_direction = None) for matrix in matrices))
 
-    def split_interaction_operators(self, mesh: 'meshes.QuantumMesh', interaction_hamiltonian, tau: complex) -> Tuple[MeshOperator, ...]:
+    def split_interaction_operators(self, mesh: 'meshes.LineMesh', interaction_hamiltonian, tau: complex) -> Tuple[MeshOperator, ...]:
         a = interaction_hamiltonian.operators[0].matrix.data[-1][1:] * tau * (-1j)  # I happen to know that it's 1D, but this is a little ugly
         len_a = len(a)
 
@@ -307,12 +307,24 @@ class LineVelocityGaugeOperators(LineLengthGaugeOperators):
 
 class CylindricalSliceLengthGaugeOperators(Operators):
     @si.utils.memoize
-    def kinetic_energy(self, mesh) -> SumOfOperators:
+    def kinetic_energy(self, mesh: 'meshes.CylindricalSliceMesh') -> SumOfOperators:
+        return SumOfOperators(
+            self.rho_kinetic_energy(mesh),
+            self.z_kinetic_energy(mesh),
+        )
+
+    def z_kinetic_energy(self, mesh: 'meshes.CylindricalSliceMesh') -> MeshOperator:
         z_prefactor = -(u.hbar ** 2) / (2 * mesh.spec.test_mass * (mesh.delta_z ** 2))
-        rho_prefactor = -(u.hbar ** 2) / (2 * mesh.spec.test_mass * (mesh.delta_rho ** 2))
 
         z_diagonal = z_prefactor * (-2) * np.ones(mesh.mesh_points, dtype = np.complex128)
         z_offdiagonal = z_prefactor * np.array([1 if (z_index + 1) % mesh.spec.z_points != 0 else 0 for z_index in range(mesh.mesh_points - 1)], dtype = np.complex128)
+
+        z_kinetic = sparse.diags((z_offdiagonal, z_diagonal, z_offdiagonal), offsets = (-1, 0, 1))
+
+        return DotOperator(z_kinetic, wrapping_direction = meshes.WrappingDirection.Z)
+
+    def rho_kinetic_energy(self, mesh: 'meshes.CylindricalSliceMesh') -> MeshOperator:
+        rho_prefactor = -(u.hbar ** 2) / (2 * mesh.spec.test_mass * (mesh.delta_rho ** 2))
 
         def c(j):
             return j / np.sqrt((j ** 2) - 0.25)
@@ -325,15 +337,11 @@ class CylindricalSliceLengthGaugeOperators(Operators):
                 rho_offdiagonal[rho_index] = c(j)
         rho_offdiagonal *= rho_prefactor
 
-        z_kinetic = sparse.diags((z_offdiagonal, z_diagonal, z_offdiagonal), offsets = (-1, 0, 1))
         rho_kinetic = sparse.diags((rho_offdiagonal, rho_diagonal, rho_offdiagonal), offsets = (-1, 0, 1))
 
-        return SumOfOperators(
-            DotOperator(rho_kinetic, wrapping_direction = meshes.WrappingDirection.RHO),
-            DotOperator(z_kinetic, wrapping_direction = meshes.WrappingDirection.Z),
-        )
+        return DotOperator(rho_kinetic, wrapping_direction = meshes.WrappingDirection.RHO)
 
-    def interaction_hamiltonian(self, mesh) -> SumOfOperators:
+    def interaction_hamiltonian(self, mesh: 'meshes.CylindricalSliceMesh') -> SumOfOperators:
         electric_potential_energy_mesh = mesh.spec.electric_potential(t = mesh.sim.time, distance_along_polarization = mesh.z_mesh, test_charge = mesh.spec.test_charge)
 
         interaction_hamiltonian_z = sparse.diags(mesh.flatten_mesh(electric_potential_energy_mesh, meshes.WrappingDirection.Z))
@@ -345,36 +353,44 @@ class CylindricalSliceLengthGaugeOperators(Operators):
         )
 
     @si.utils.memoize
-    def r(self, mesh) -> SumOfOperators:
+    def r(self, mesh: 'meshes.CylindricalSliceMesh') -> SumOfOperators:
         return SumOfOperators(ElementWiseMultiplyOperator(mesh.r_mesh))
 
     @si.utils.memoize
-    def z(self, mesh) -> SumOfOperators:
+    def z(self, mesh: 'meshes.CylindricalSliceMesh') -> SumOfOperators:
         return SumOfOperators(ElementWiseMultiplyOperator(mesh.z_mesh))
 
 
 class SphericalSliceLengthGaugeOperators(Operators):
     @si.utils.memoize
-    def kinetic_energy(self, mesh) -> SumOfOperators:
+    def kinetic_energy(self, mesh: 'meshes.SphericalSliceMesh') -> SumOfOperators:
+        return SumOfOperators(
+            self.theta_kinetic_energy(mesh),
+            self.r_kinetic_energy(mesh),
+        )
+
+    def r_kinetic_energy(self, mesh: 'meshes.SphericalSliceMesh'):
         r_prefactor = -(u.hbar ** 2) / (2 * u.electron_mass_reduced * (mesh.delta_r ** 2))
-        theta_prefactor = -(u.hbar ** 2) / (2 * u.electron_mass_reduced * ((mesh.delta_r * mesh.delta_theta) ** 2))
 
         r_diagonal = -2 * r_prefactor * np.ones(mesh.mesh_points, dtype = np.complex128)
         r_offdiagonal = r_prefactor * np.array([1 if (z_index + 1) % mesh.spec.r_points != 0 else 0 for z_index in range(mesh.mesh_points - 1)], dtype = np.complex128)
 
-        @si.utils.memoize
+        r_kinetic = sparse.diags((r_offdiagonal, r_diagonal, r_offdiagonal), offsets = (-1, 0, 1))
+
+        return DotOperator(r_kinetic, wrapping_direction = meshes.WrappingDirection.R)
+
+    def theta_kinetic_energy(self, mesh: 'meshes.SphericalSliceMesh') -> SumOfOperators:
+        theta_prefactor = -(u.hbar ** 2) / (2 * u.electron_mass_reduced * ((mesh.delta_r * mesh.delta_theta) ** 2))
+
         def theta_j_prefactor(x):
             return 1 / (x + 0.5) ** 2
 
-        @si.utils.memoize
         def sink(x):
             return np.sin(x * mesh.delta_theta)
 
-        @si.utils.memoize
         def sqrt_sink_ratio(x_num, x_den):
             return np.sqrt(sink(x_num) / sink(x_den))
 
-        @si.utils.memoize
         def cotank(x):
             return 1 / np.tan(x * mesh.delta_theta)
 
@@ -396,15 +412,11 @@ class SphericalSliceLengthGaugeOperators(Operators):
         theta_upper_diagonal *= theta_prefactor
         theta_lower_diagonal *= theta_prefactor
 
-        r_kinetic = sparse.diags((r_offdiagonal, r_diagonal, r_offdiagonal), offsets = (-1, 0, 1))
         theta_kinetic = sparse.diags((theta_lower_diagonal, theta_diagonal, theta_upper_diagonal), offsets = (-1, 0, 1))
 
-        return SumOfOperators(
-            DotOperator(theta_kinetic, wrapping_direction = meshes.WrappingDirection.THETA),
-            DotOperator(r_kinetic, wrapping_direction = meshes.WrappingDirection.R),
-        )
+        return DotOperator(theta_kinetic, wrapping_direction = meshes.WrappingDirection.THETA)
 
-    def interaction_hamiltonian(self, mesh) -> SumOfOperators:
+    def interaction_hamiltonian(self, mesh: 'meshes.SphericalSliceMesh') -> SumOfOperators:
         electric_potential_energy_mesh = mesh.spec.electric_potential(t = mesh.sim.time, distance_along_polarization = mesh.z_mesh, test_charge = mesh.spec.test_charge)
 
         interaction_hamiltonian_r = sparse.diags(mesh.flatten_mesh(electric_potential_energy_mesh, meshes.WrappingDirection.R))
@@ -436,27 +448,27 @@ class SphericalHarmonicLengthGaugeOperators(Operators):
 
         return info
 
-    def alpha(self, j) -> float:
+    def alpha(self, j) -> Union[float, np.array]:
         x = (j ** 2) + (2 * j)
         return (x + 1) / (x + 0.75)
 
-    def beta(self, j) -> float:
+    def beta(self, j) -> Union[float, np.array]:
         x = 2 * (j ** 2) + (2 * j)
         return (x + 1) / (x + 0.5)
 
-    def gamma(self, j) -> float:
+    def gamma(self, j) -> Union[float, np.array]:
         """For radial probability current."""
         return 1 / ((j ** 2) - 0.25)
 
-    def c_l(self, l) -> float:
+    def c_l(self, l) -> Union[float, np.array]:
         """a particular set of 3j coefficients for SphericalHarmonicMesh"""
         return (l + 1) / np.sqrt(((2 * l) + 1) * ((2 * l) + 3))
 
     @si.utils.memoize
-    def kinetic_energy(self, mesh) -> SumOfOperators:
+    def kinetic_energy(self, mesh: 'meshes.SphericalHarmonicMesh') -> SumOfOperators:
         return getattr(self, f'kinetic_energy_from_{self.kinetic_energy_derivation}')(mesh)
 
-    def kinetic_energy_from_hamiltonian(self, mesh) -> SumOfOperators:
+    def kinetic_energy_from_hamiltonian(self, mesh: 'meshes.SphericalHarmonicMesh') -> SumOfOperators:
         r_prefactor = -(u.hbar ** 2) / (2 * u.electron_mass_reduced * (mesh.delta_r ** 2))
 
         r_diagonal = r_prefactor * (-2) * np.ones(mesh.mesh_points, dtype = np.complex128)
@@ -469,7 +481,7 @@ class SphericalHarmonicLengthGaugeOperators(Operators):
 
         return SumOfOperators(DotOperator(r_kinetic, wrapping_direction = meshes.WrappingDirection.R))
 
-    def kinetic_energy_from_lagrangian(self, mesh) -> SumOfOperators:
+    def kinetic_energy_from_lagrangian(self, mesh: 'meshes.SphericalHarmonicMesh') -> SumOfOperators:
         r_prefactor = -(u.hbar ** 2) / (2 * u.electron_mass_reduced * (mesh.delta_r ** 2))
 
         r_diagonal = np.zeros(mesh.mesh_points, dtype = np.complex128)
@@ -495,13 +507,13 @@ class SphericalHarmonicLengthGaugeOperators(Operators):
 
         return SumOfOperators(DotOperator(r_kinetic, wrapping_direction = meshes.WrappingDirection.R))
 
-    def kinetic_energy_for_single_l(self, mesh: 'meshes.QuantumMesh', l: core.AngularMomentum) -> MeshOperator:
+    def kinetic_energy_for_single_l(self, mesh: 'meshes.SphericalHarmonicMesh', l: core.AngularMomentum) -> MeshOperator:
         return getattr(self, f'kinetic_energy_for_single_l_from_{self.kinetic_energy_derivation}')(mesh, l)
 
-    def kinetic_energy_for_single_l_from_hamiltonian(self, mesh: 'meshes.QuantumMesh', l: core.AngularMomentum) -> MeshOperator:
+    def kinetic_energy_for_single_l_from_hamiltonian(self, mesh: 'meshes.SphericalHarmonicMesh', l: core.AngularMomentum) -> MeshOperator:
         raise NotImplementedError
 
-    def kinetic_energy_for_single_l_from_lagrangian(self, mesh: 'meshes.QuantumMesh', l: core.AngularMomentum) -> MeshOperator:
+    def kinetic_energy_for_single_l_from_lagrangian(self, mesh: 'meshes.SphericalHarmonicMesh', l: core.AngularMomentum) -> MeshOperator:
         r_prefactor = -(u.hbar ** 2) / (2 * u.electron_mass_reduced * (mesh.delta_r ** 2))
         effective_potential = ((u.hbar ** 2) / (2 * u.electron_mass_reduced)) * l * (l + 1) / (mesh.r ** 2)
 
@@ -516,7 +528,7 @@ class SphericalHarmonicLengthGaugeOperators(Operators):
 
         return DotOperator(matrix, wrapping_direction = meshes.WrappingDirection.R)
 
-    def internal_hamiltonian_for_single_l(self, mesh: 'meshes.QuantumMesh', l: core.AngularMomentum) -> MeshOperator:
+    def internal_hamiltonian_for_single_l(self, mesh: 'meshes.SphericalHarmonicMesh', l: core.AngularMomentum) -> MeshOperator:
         kinetic_operator = self.kinetic_energy_for_single_l(mesh, l)
         potential_mesh = mesh.spec.internal_potential(r = mesh.r, test_charge = mesh.spec.test_charge)  # evaluate at r, not r_mesh, because it's only one l
 
@@ -567,7 +579,7 @@ class SphericalHarmonicLengthGaugeOperators(Operators):
 
         return SumOfOperators(*internal, *interaction)
 
-    def split_interaction_operators(self, mesh: 'meshes.QuantumMesh', interaction_operators, tau: complex) -> Tuple[MeshOperator, ...]:
+    def split_interaction_operators(self, mesh: 'meshes.SphericalHarmonicMesh', interaction_operators, tau: complex) -> Tuple[MeshOperator, ...]:
         interaction_operator = interaction_operators.operators[0]  # only one operator in length gauge
         a = tau * interaction_operator.matrix.data[0][:-1]
 
@@ -609,11 +621,11 @@ class SphericalHarmonicLengthGaugeOperators(Operators):
         )
 
     @si.utils.memoize
-    def r(self, mesh) -> SumOfOperators:
+    def r(self, mesh: 'meshes.SphericalHarmonicMesh') -> SumOfOperators:
         return SumOfOperators(ElementWiseMultiplyOperator(mesh.r_mesh))
 
     @si.utils.memoize
-    def z(self, mesh) -> SumOfOperators:
+    def z(self, mesh: 'meshes.SphericalHarmonicMesh') -> SumOfOperators:
         l_prefactor = mesh.flatten_mesh(mesh.r_mesh, 'l')[:-1]
 
         l_diagonal = np.zeros(mesh.mesh_points, dtype = np.complex128)
@@ -630,11 +642,11 @@ class SphericalHarmonicLengthGaugeOperators(Operators):
 
 
 class SphericalHarmonicVelocityGaugeOperators(SphericalHarmonicLengthGaugeOperators):
-    def __init__(self, hydrogen_zero_angular_momentum_correction = True):
+    def __init__(self, hydrogen_zero_angular_momentum_correction: bool = True):
         super().__init__(hydrogen_zero_angular_momentum_correction = hydrogen_zero_angular_momentum_correction)
 
     @si.utils.memoize
-    def interaction_hamiltonian_matrices_without_field(self, mesh) -> Tuple['meshes.OperatorMatrix', ...]:
+    def interaction_hamiltonian_matrices_without_field(self, mesh: 'meshes.SphericalHarmonicMesh') -> Tuple['meshes.OperatorMatrix', ...]:
         h1_prefactor = 1j * u.hbar * (mesh.spec.test_charge / mesh.spec.test_mass) / mesh.flatten_mesh(mesh.r_mesh, 'l')[:-1]
 
         h1_offdiagonal = np.zeros(mesh.mesh_points - 1, dtype = np.complex128)
@@ -658,13 +670,13 @@ class SphericalHarmonicVelocityGaugeOperators(SphericalHarmonicLengthGaugeOperat
 
         return h1, h2
 
-    def interaction_hamiltonian(self, mesh) -> SumOfOperators:
+    def interaction_hamiltonian(self, mesh: 'meshes.SphericalHarmonicMesh') -> SumOfOperators:
         h1, h2 = self.interaction_hamiltonian_matrices_without_field(mesh)
         vector_potential_amp = mesh.spec.electric_potential.get_vector_potential_amplitude_numeric(mesh.sim.times_to_current)  # decouple
 
         return SumOfOperators(vector_potential_amp * h1, vector_potential_amp * h2)
 
-    def split_interaction_operators(self, mesh: 'meshes.QuantumMesh', interaction_hamiltonian: SumOfOperators, tau: complex) -> Tuple[MeshOperator, ...]:
+    def split_interaction_operators(self, mesh: 'meshes.SphericalHarmonicMesh', interaction_hamiltonian: SumOfOperators, tau: complex) -> Tuple[MeshOperator, ...]:
         h1, h2 = self.interaction_hamiltonian(mesh).operators
 
         h1_operators = self.split_h1(mesh, h1, tau)
@@ -672,7 +684,7 @@ class SphericalHarmonicVelocityGaugeOperators(SphericalHarmonicLengthGaugeOperat
 
         return (*h1_operators, *h2_operators)
 
-    def split_h1(self, mesh: 'meshes.QuantumMesh', h1, tau: complex) -> Tuple[MeshOperator, ...]:
+    def split_h1(self, mesh: 'meshes.SphericalHarmonicMesh', h1, tau: complex) -> Tuple[MeshOperator, ...]:
         a = (tau * (-1j)) * h1.data[-1][1:]
 
         a_even, a_odd = a[::2], a[1::2]
@@ -710,7 +722,7 @@ class SphericalHarmonicVelocityGaugeOperators(SphericalHarmonicLengthGaugeOperat
             DotOperator(odd, wrapping_direction = meshes.WrappingDirection.L),
         )
 
-    def split_h2(self, mesh: 'meshes.QuantumMesh', h2, tau: complex) -> Tuple[MeshOperator, ...]:
+    def split_h2(self, mesh: 'meshes.SphericalHarmonicMesh', h2, tau: complex) -> Tuple[MeshOperator, ...]:
         len_r = len(mesh.r)
 
         a = h2.data[-1][len_r + 1:] * tau * (-1j)

src/ionization/tunneling/models.py
@@ -28,6 +28,11 @@ class TunnelingModel(abc.ABC):
         return info
 
 
+class NoTunneling(TunnelingModel):
+    def tunneling_rate_from_amplitude(self, electric_field_amplitude, ionization_potential):
+        return 0
+
+
 # the following rate calculations are from
 # https://doi.org/10.1103/PhysRevA.59.569
 
@@ -40,13 +45,13 @@ class LandauRate(TunnelingModel):
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')  # ignore div by zero errors
 
-            rate = -(4 / f) * np.exp(-2 / (3 * f)) / u.atomic_time,
+            rate = -(4 * (2 * scaled_potential) / f) * np.exp(-2 / (3 * f)) / u.atomic_time,
 
         rate = np.where(
             np.isclose(scaled_amplitude, 0),
             0,
             rate,
-        )
+        ).squeeze()
 
         return rate
 
@@ -61,13 +66,13 @@ class KeldyshRate(TunnelingModel):
             warnings.simplefilter('ignore')  # ignore div by zero errors
 
             pre = (np.sqrt(6 * u.pi) / (2 ** 1.25))
-            rate = pre * scaled_potential * np.sqrt(f) * np.exp(-2 / (3 * f)) / u.atomic_time
+            rate = -pre * scaled_potential * np.sqrt(f) * np.exp(-2 / (3 * f)) / u.atomic_time
 
         rate = np.where(
             np.isclose(scaled_amplitude, 0),
             0,
             rate,
-        )
+        ).squeeze()
 
         return rate
 
@@ -89,13 +94,13 @@ class PosthumusRate(TunnelingModel):
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')  # ignore div by zero errors
 
-            rate = (1 - ((scaled_potential ** 2) / (4 * self.atomic_number * scaled_amplitude))) / (2 * T) / u.atomic_time
+            rate = -(1 - ((scaled_potential ** 2) / (4 * self.atomic_number * scaled_amplitude))) / (2 * T) / u.atomic_time
 
         rate = np.where(
             np.less_equal(scaled_amplitude, 0.0625),
             0,
             rate,
-        )
+        ).squeeze()
 
         return rate
 
@@ -113,13 +118,13 @@ class MulserRate(TunnelingModel):
             beta = (3 + alpha) * C / 4
             A = np.exp(-(7 - (3 * alpha)) * C / 4)
 
-            rate = (scaled_potential / np.abs(beta)) * np.log((A + np.exp(np.abs(beta))) / (A + 1)) / u.atomic_time
+            rate = -(scaled_potential / np.abs(beta)) * np.log((A + np.exp(np.abs(beta))) / (A + 1)) / u.atomic_time
 
         rate = np.where(
             np.isclose(scaled_amplitude, 0),
             0,
             rate,
-        )
+        ).squeeze()
 
         return rate
 
@@ -158,13 +163,13 @@ class ADKRate(TunnelingModel):
             C_nl = ((2 * u.e / self.n_star) ** self.n_star) / np.sqrt(u.twopi * self.n_star)
             pre = (C_nl ** 2) * f_lm
 
-            rate = pre * scaled_potential * np.sqrt(3 * f / u.pi) * ((2 / f) ** (2 * self.n_star - np.abs(self.m) - 1)) * np.exp(-2 / (3 * f)) / u.atomic_time
+            rate = -pre * scaled_potential * np.sqrt(3 * f / u.pi) * ((2 / f) ** (2 * self.n_star - np.abs(self.m) - 1)) * np.exp(-2 / (3 * f)) / u.atomic_time
 
         rate = np.where(
             np.isclose(scaled_amplitude, 0),
             0,
             rate,
-        )
+        ).squeeze()
 
         return rate
 
@@ -203,12 +208,12 @@ class ADKExtendedToBSIRate(ADKRate):
             except TypeError:
                 integral = np.array([integ.quad(functools.partial(integrand, amplitude = a), 0, np.inf)[0] for a in scaled_amplitude])
 
-            rate = pre * factor_1 * factor_2 * integral / u.atomic_time
+            rate = -pre * factor_1 * factor_2 * integral / u.atomic_time
 
         rate = np.where(
             np.isclose(scaled_amplitude, 0),
             0,
             rate,
-        )
+        ).squeeze()
 
         return rate

src/ionization/tunneling/tunneling.py
@@ -52,7 +52,7 @@ class TunnelingSimulation(si.Simulation):
         self.status = si.Status.RUNNING
 
         if progress_bar:
-            pbar = tqdm(total = self.time_steps - 1, ascii = True)
+            pbar = tqdm(total = self.time_steps - 1, ascii = True, ncols = 80)
 
         while True:
             if callback is not None:
@@ -64,7 +64,7 @@ class TunnelingSimulation(si.Simulation):
             self.time_index += 1
 
             dt = self.times[self.time_index] - self.times[self.time_index - 1]
-            tunneling_rate = self.spec.model.tunneling_rate(self, self.spec.electric_potential, self.time + (dt / 2))
+            tunneling_rate = self.spec.tunneling_model.tunneling_rate(self, self.spec.electric_potential, self.time + (dt / 2))
             self.b[self.time_index] = self.b[self.time_index - 1] * np.exp(tunneling_rate * dt)
 
             logger.debug(f'{self.__class__.__name__} {self.name} ({self.file_name}) evolved to time index {self.time_index} / {self.time_steps - 1} ({np.around(100 * (self.time_index + 1) / self.time_steps, 2)}%)')
@@ -94,7 +94,7 @@ class TunnelingSpecification(si.Specification):
             time_step: float = 1 * u.asec,
             electric_potential = potentials.NoElectricPotential(),
             electric_potential_dc_correction = False,
-            model: models.TunnelingModel = models.LandauRate(),
+            tunneling_model: models.TunnelingModel = models.LandauRate(),
             ionization_potential: float = -u.rydberg,
             b_initial: complex = 1,
             **kwargs):
@@ -107,7 +107,7 @@ class TunnelingSpecification(si.Specification):
         self.electric_potential = electric_potential
         self.electric_potential_dc_correction = electric_potential_dc_correction
 
-        self.model = model
+        self.tunneling_model = tunneling_model
 
         self.ionization_potential = ionization_potential
         self.b_initial = b_initial
@@ -121,7 +121,7 @@ class TunnelingSpecification(si.Specification):
         info_evolution.add_field('Time Step', f'{u.uround(self.time_step, u.asec, 3)} as | {u.uround(self.time_step, u.atomic_time, 3)} a.u.')
         info.add_info(info_evolution)
 
-        info.add_info(self.model.info())
+        info.add_info(self.tunneling_model.info())
 
         info.add_field('Initial b', self.b_initial)
 