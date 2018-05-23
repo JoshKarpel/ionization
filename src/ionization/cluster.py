7e93e25526cab94b3b5fc0d58bd668914bb14d9f
JoshKarpel <josh.karpel@gmail.com>
2018-05-22 23:19:12 -0500

    no 'vs_time' in .data accesses

--- src/ionization/cluster.py
+++ src/ionization/cluster.py
@@ -376,9 +376,9 @@ class MeshSimulationResult(PulseSimulationResult):
 
         self.time_steps = copy(sim.time_steps)
 
-        state_overlaps = sim.data.state_overlaps_vs_time
+        state_overlaps = sim.data.state_overlaps
 
-        self.final_norm = copy(sim.data.norm_vs_time[-1])
+        self.final_norm = copy(sim.data.norm[-1])
         self.final_initial_state_overlap = copy(state_overlaps[sim.spec.initial_state][-1])
         self.final_bound_state_overlap = copy(sum(state_overlaps[s][-1] for s in sim.bound_states))
         self.final_free_state_overlap = copy(sum(state_overlaps[s][-1] for s in sim.free_states))
