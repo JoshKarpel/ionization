d486040fd9c95543dddc78ad489ad8c0e081dc84
JoshKarpel <josh.karpel@gmail.com>
2018-06-21 14:06:29 -0500

    refactored run_sim.py

--- scripts/chtc/running_sims/run_sim.py
+++ scripts/chtc/running_sims/run_sim.py
@@ -5,6 +5,7 @@ import socket
 import datetime
 import argparse
 import logging
+from pathlib import Path
 
 import simulacra as si
 
@@ -17,7 +18,24 @@ def ensure_compatibility_sim(sim):
     pass
 
 
-if __name__ == '__main__':
+def find_or_init_sim(sim_name, logger):
+    try:
+        sim_path = Path.cwd() / f'{sim_name}.sim'
+        sim = si.Simulation.load(sim_path)
+        ensure_compatibility_spec(sim.spec)
+        ensure_compatibility_sim(sim)
+        logger.info(f'Checkpoint found at {sim_path}, recovered simulation {sim}')
+        logger.info(f'Checkpoint size is {si.utils.get_file_size_as_string(sim_path)}')
+    except (FileNotFoundError, EOFError):
+        spec = si.Specification.load(os.path.join(os.getcwd(), f'{sim_name}.spec'))
+        ensure_compatibility_spec(spec)
+        sim = spec.to_sim()
+        logger.info(f'Checkpoint not found, started simulation {sim}')
+
+    return sim
+
+
+def parse_args():
     parser = argparse.ArgumentParser(description = 'Run a simulation.')
     parser.add_argument(
         'sim_name',
@@ -26,6 +44,12 @@ if __name__ == '__main__':
     )
 
     args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
 
     logman = si.utils.LogManager(
         '__main__', 'simulacra', 'ionization',
@@ -37,24 +61,14 @@ if __name__ == '__main__':
     )
     with logman as logger:
         try:
-            logger.info(f'Loaded onto execute node {socket.getfqdn()} (IP {socket.gethostbyname(socket.gethostname())}) at {datetime.datetime.utcnow()}.')
+            logger.info(f'Landed on execute node {socket.getfqdn()} ({socket.gethostbyname(socket.gethostname())}) at {datetime.datetime.utcnow()}.')
             logger.info(f'Execute node operating system: {os.uname()}')
-            logger.info(f'Local directory contents: {os.listdir(os.getcwd())}')
 
-            try:
-                sim_path = os.path.join(os.getcwd(), '{}.sim'.format(args.sim_name))
-                sim = si.Simulation.load(sim_path)
-                ensure_compatibility_spec(sim.spec)
-                ensure_compatibility_sim(sim)
-                logger.info(f'Checkpoint found at {sim_path}, recovered simulation {sim}')
-                logger.info(f'Checkpoint size is {si.utils.get_file_size_as_string(sim_path)}')
-            except (FileNotFoundError, EOFError):
-                spec = si.Specification.load(os.path.join(os.getcwd(), f'{args.sim_name}.spec'))
-                ensure_compatibility_spec(spec)
-                sim = spec.to_sim()
-                logger.info(f'Checkpoint not found, started simulation {sim}')
+            dir_contents = ", ".join(str(x) for x in Path.cwd().iterdir())
+            logger.info(f'Local directory contents: {dir_contents}')
 
-            # run the simulation and save it
+            sim = find_or_init_sim(sim_name = args.sim_name, logger = logger)
+
             logger.info(sim.info())
             sim.run()
             logger.info(sim.info())
@@ -63,3 +77,7 @@ if __name__ == '__main__':
         except Exception as e:
             logger.exception(e)
             raise e
+
+
+if __name__ == '__main__':
+    main()
