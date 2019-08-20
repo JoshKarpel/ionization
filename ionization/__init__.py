df0e03e2ffeffb4d8d1f48134c45b05f22899a80
JoshKarpel <josh.karpel@gmail.com>
2019-08-20 10:52:22 -0500

    add versioning framework

--- ionization/__init__.py
+++ ionization/__init__.py
@@ -5,13 +5,20 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.NullHandler())
 
+import os as _os
+
+# see https://github.com/ContinuumIO/anaconda-issues/issues/905
+_os.environ["FOR_DISABLE_CONSOLE_CTRL_HANDLER"] = "1"
+
+from .version import __version__, version, version_info
+
 # set up platform-independent runtime cython compilation and imports
 import numpy as _np
-import pyximport
+import pyximport as _pyximport
 
-pyx_dir = os.path.join(os.path.dirname(__file__), ".pyxbld")
-pyximport.install(
-    setup_args={"include_dirs": _np.get_include()}, build_dir=pyx_dir, language_level=3
+_pyx_dir = os.path.join(os.path.dirname(__file__), ".pyxbld")
+_pyximport.install(
+    setup_args={"include_dirs": _np.get_include()}, build_dir=_pyx_dir, language_level=3
 )
 
 from . import mesh, ide, tunneling, potentials, states, exceptions, analysis
