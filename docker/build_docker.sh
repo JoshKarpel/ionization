d52b3147542f6b8a562ff332d74459f2e0d3cedc
JoshKarpel <josh.karpel@gmail.com>
2019-09-20 09:18:34 -0500

    de-personalize docker build script

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-USERNAME=maventree
-IMAGE=ionization
+TAG=$1
 
-docker build --pull --build-arg CACHEBUST=$(date +%s) -t ${USERNAME}/${IMAGE}:$1 .
-docker push ${USERNAME}/${IMAGE}:$1
+docker build --pull --build-arg CACHEBUST="$(date +%s)" -t "${TAG}" .
+docker push "${TAG}"
